2313e74338717aee9c8ed151ecd05c86bc0191c0
Sanja Panic <gamesg274@gmail.com>
2023-04-30 13:57:49 +0200

    handle adding events to bookmark

--- pages/events/index.vue
+++ pages/events/index.vue
@@ -29,7 +29,7 @@
                 fill="red"
                 height="20"
                 width="20"
-                @click="isInBookmark(event.id)"
+                @click="updateUserBookmark(event.id)"
               >
                 <path
                   d="M11.645 20.91l-.007-.003-.022-.012a15.247 15.247 0 01-.383-.218 25.18 25.18 0 01-4.244-3.17C4.688 15.36 2.25 12.174 2.25 8.25 2.25 5.322 4.714 3 7.688 3A5.5 5.5 0 0112 5.052 5.5 5.5 0 0116.313 3c2.973 0 5.437 2.322 5.437 5.25 0 3.925-2.438 7.111-4.739 9.256a25.175 25.175 0 01-4.244 3.17 15.247 15.247 0 01-.383.219l-.022.012-.007.004-.003.001a.752.752 0 01-.704 0l-.003-.001z"
@@ -44,7 +44,7 @@
                 stroke="red"
                 height="20"
                 width="20"
-                @click="isInBookmark(event.id)"
+                @click="updateUserBookmark(event.id)"
               >
                 <path
                   strokeLinecap="round"
@@ -115,14 +115,68 @@ export default {
         for (const bookmark of doc.bookmark) {
           if (bookmark === eventId) {
             return true
-          } else {
-            return false
           }
         }
       }
+
+      return false
     },
-    async updateUserBookmark(eventId) {
-      // todo
+    updateUserBookmark(eventId) {
+      const db = firebase.firestore()
+      const user = firebase.auth().currentUser
+
+      const userBookmarkRef = db.collection('users').doc(user.uid)
+
+      if (this.isInBookmark(eventId)) {
+        userBookmarkRef
+          .update({
+            bookmark: firebase.firestore.FieldValue.arrayRemove(eventId),
+          })
+          .then(async () => {
+            // get user bookmark data
+            const bookmarkSnapshot = await db.collection('users').get()
+
+            if (bookmarkSnapshot.size > 0) {
+              for (const doc of bookmarkSnapshot.docs) {
+                if (doc.id === user.uid) {
+                  this.userBookmarks.push({
+                    ...doc.data(),
+                  })
+                }
+              }
+            }
+
+            this.isInBookmark(eventId)
+            this.$nuxt.refresh()
+          })
+          .catch((error) => {
+            this.error = error.message
+          })
+      } else {
+        userBookmarkRef
+          .update({
+            bookmark: firebase.firestore.FieldValue.arrayUnion(eventId),
+          })
+          .then(async () => {
+            // get user bookmark data
+            const bookmarkSnapshot = await db.collection('users').get()
+
+            if (bookmarkSnapshot.size > 0) {
+              for (const doc of bookmarkSnapshot.docs) {
+                if (doc.id === user.uid) {
+                  this.userBookmarks.push({
+                    ...doc.data(),
+                  })
+                }
+              }
+            }
+
+            this.isInBookmark(eventId)
+          })
+          .catch((error) => {
+            this.error = error.message
+          })
+      }
     },
   },
 }
